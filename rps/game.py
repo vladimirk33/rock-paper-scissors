3960f5d939b8f2ffe93fe96fb4d03ebdda4495f9
Vladimir <mr.katenin@mail.ru>
2020-10-19 23:40:20 +0300

    added rating

--- rps/game.py
+++ rps/game.py
@@ -9,9 +9,44 @@ LOSES = (("rock", "paper"),
          ("scissors", "rock"))
 
 
+def read_player_score(name):
+    file = open("rating.txt", "r+", encoding="utf-8")
+    for player in file.readlines():
+        player = player.split()
+        if player[0] == name:
+            file.close()
+            return player[1]
+    file.write(f"{name} 0\n")
+    file.close()
+
+
+def write_player_score(name, mode):
+    reading_file = open("rating.txt", "r")
+    new_file_content = ""
+    for line in reading_file:
+        stripped_line = line.strip()
+        player = stripped_line.split()
+        if player[0] == name:
+            delta = 0
+            if mode == "win":
+                delta = 100
+            elif mode == "tie":
+                delta = 50
+            new_line = stripped_line.replace(f"{player[0]} {player[1]}",
+                                             f"{player[0]} {int(player[1]) + delta}")
+        else:
+            new_line = stripped_line
+        new_file_content += new_line + "\n"
+    reading_file.close()
+
+    writing_file = open("rating.txt", "w")
+    writing_file.write(new_file_content)
+    writing_file.close()
+
+
 def human_move():
     user_move = input()
-    while user_move not in ("rock", "paper", "scissors", "!exit"):
+    while user_move not in ("rock", "paper", "scissors", "!rating", "!exit"):
         print("Invalid input")
         user_move = input()
     return user_move
@@ -22,23 +57,32 @@ def computer_move():
     return ai_move
 
 
-def choose_winner(human_move, computer_move):
+def choose_winner(human_move, computer_move, name):
     for human, computer in WINS:
         if human_move == human and computer_move == computer:
             print(f"Well done. The computer chose {computer_move} and failed")
+            mode = "win"
+            write_player_score(name, mode)
     for human, computer in LOSES:
         if human_move == human and computer_move == computer:
             print(f"Sorry, but the computer chose {computer_move}")
     if human_move == computer_move:
         print(f"There is a draw ({computer_move})")
+        mode = "tie"
+        write_player_score(name, mode)
 
 
 def main():
+    name = input("Enter your name: ")
+    print("Hello,", name)
+    read_player_score(name)
     human = None
     while human != "!exit":
+        if human == "!rating":
+            print("Your rating:", read_player_score(name))
         human = human_move()
         computer = computer_move()
-        choose_winner(human, computer)
+        choose_winner(human, computer, name)
     print("Bye!")
 
 
